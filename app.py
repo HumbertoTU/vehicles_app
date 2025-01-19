e73cb8378b16677501ce76c7bdb70c3d688bf78c
HumbertoTU <humberto.TU@outlook.com>
2025-01-19 16:45:03 -0600

    add notes file

--- app.py
+++ app.py
@@ -10,32 +10,24 @@ data_path = os.path.join(script_dir, 'notebooks', 'vehicles_us.csv')
 # Cargar los datos
 car_data = pd.read_csv(data_path)
 
-# Crear el encabezado
-st.header('Aplicación de Visualización de Datos de Vehículos')
+# Título de la app
+st.title('Aplicación de Análisis de Datos de Vehículos')
 
-# Crear las casillas de verificación para seleccionar los gráficos
-show_histogram = st.checkbox('Mostrar histograma de odómetro')
-show_scatter = st.checkbox('Mostrar gráfico de dispersión')
+# Descripción breve del objetivo de la app
+st.write("""
+Esta aplicación permite explorar y analizar un conjunto de datos sobre anuncios de vehículos en venta. 
+Puedes visualizar estadísticas descriptivas, crear gráficos interactivos y obtener información valiosa sobre los vehículos disponibles.
+""")
 
-# Si el usuario selecciona mostrar el histograma
-if show_histogram:
-    st.write('Creando un histograma para el odómetro de los vehículos')
+# Título para la sección de visualización de datos
+st.subheader('Visualización de los Datos de Vehículos')
 
-    # Crear el histograma
-    fig_hist = px.histogram(car_data, x="odometer",
-                            title="Histograma de Odómetro")
 
-    # Mostrar el histograma interactivo
-    st.plotly_chart(fig_hist, use_container_width=True)
+# Checkbox para decidir si mostrar el DataFrame
+show_data = st.checkbox('Mostrar datos de vehículos')
 
-# Si el usuario selecciona mostrar el gráfico de dispersión
-if show_scatter:
-    st.write(
-        'Creando un gráfico de dispersión entre el precio y el odómetro de los vehículos')
-
-    # Crear el gráfico de dispersión
-    fig_scatter = px.scatter(car_data, x="odometer", y="price",
-                             title="Gráfico de Dispersión: Precio vs Odómetro")
-
-    # Mostrar el gráfico de dispersión interactivo
-    st.plotly_chart(fig_scatter, use_container_width=True)
+# Si el checkbox está seleccionado, mostrar el DataFrame
+if show_data:
+    # Mostrar todo el DataFrame con barras de desplazamiento
+    # Ajusta 'height' según lo necesites para reducir el tamaño
+    st.dataframe(car_data, height=200)
