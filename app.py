538134dc595b2d59c940cbded078ce844fa3efa8
HumbertoTU <humberto.TU@outlook.com>
2025-01-19 16:03:17 -0600

    add notes file

--- app.py
+++ app.py
@@ -10,12 +10,32 @@ data_path = os.path.join(script_dir, 'notebooks', 'vehicles_us.csv')
 # Cargar los datos
 car_data = pd.read_csv(data_path)
 
-# Título de la app
-st.title("Análisis de Vehículos en Venta")
+# Crear el encabezado
+st.header('Aplicación de Visualización de Datos de Vehículos')
 
-# Botón para graficar
-if st.button('Construir Histograma'):
-    st.write("Histograma de kilometraje de los vehículos en venta")
-    fig = px.histogram(car_data, x="odometer",
-                       title="Kilometraje de Vehículos")
-    st.plotly_chart(fig, use_container_width=True)
+# Crear las casillas de verificación para seleccionar los gráficos
+show_histogram = st.checkbox('Mostrar histograma de odómetro')
+show_scatter = st.checkbox('Mostrar gráfico de dispersión')
+
+# Si el usuario selecciona mostrar el histograma
+if show_histogram:
+    st.write('Creando un histograma para el odómetro de los vehículos')
+
+    # Crear el histograma
+    fig_hist = px.histogram(car_data, x="odometer",
+                            title="Histograma de Odómetro")
+
+    # Mostrar el histograma interactivo
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# Si el usuario selecciona mostrar el gráfico de dispersión
+if show_scatter:
+    st.write(
+        'Creando un gráfico de dispersión entre el precio y el odómetro de los vehículos')
+
+    # Crear el gráfico de dispersión
+    fig_scatter = px.scatter(car_data, x="odometer", y="price",
+                             title="Gráfico de Dispersión: Precio vs Odómetro")
+
+    # Mostrar el gráfico de dispersión interactivo
+    st.plotly_chart(fig_scatter, use_container_width=True)
