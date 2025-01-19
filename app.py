aff81701781c370e235f3ef845cb6bf46efcc3d5
HumbertoTU <humberto.TU@outlook.com>
2025-01-19 13:42:09 -0600

    add notes file

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import os
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Ruta al archivo CSV en el subdirectorio notebooks
+# Ajustar el directorio de trabajo al directorio donde está el script
+# Obtiene la ruta completa del archivo
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Cambia el directorio de trabajo al directorio del script
+os.chdir(script_dir)
+
+# Ahora el script siempre usará rutas relativas desde la carpeta `vehicles_app`
 car_data = pd.read_csv('notebooks/vehicles_us.csv')  # leer los datos
+
 hist_button = st.button('Construir histograma')  # crear un botón
 
 if hist_button:  # al hacer clic en el botón
