34116a083219b61d1cad03aa7d0288aafca02de7
HumbertoTU <humberto.TU@outlook.com>
2025-01-19 14:24:17 -0600

    add notes file

--- app.py
+++ app.py
@@ -3,24 +3,19 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Ajustar el directorio de trabajo al directorio donde está el script
-# Obtiene la ruta completa del archivo
+# Configurar directorio de trabajo
 script_dir = os.path.dirname(os.path.abspath(__file__))
-# Cambia el directorio de trabajo al directorio del script
-os.chdir(script_dir)
+data_path = os.path.join(script_dir, 'notebooks', 'vehicles_us.csv')
 
-# Ahora el script siempre usará rutas relativas desde la carpeta `vehicles_app`
-car_data = pd.read_csv('notebooks/vehicles_us.csv')  # leer los datos
+# Cargar los datos
+car_data = pd.read_csv(data_path)
 
-hist_button = st.button('Construir histograma')  # crear un botón
+# Título de la app
+st.title("Análisis de Vehículos en Venta")
 
-if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
-    st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-
-    # mostrar un gráfico Plotly interactivo
+# Botón para graficar
+if st.button('Construir Histograma'):
+    st.write("Histograma de kilometraje de los vehículos en venta")
+    fig = px.histogram(car_data, x="odometer",
+                       title="Kilometraje de Vehículos")
     st.plotly_chart(fig, use_container_width=True)
